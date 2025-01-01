bbc41e718b699f8177280df6df890dd12c19e470
Sanjay Kumar <68441565+sanjay-kumar97@users.noreply.github.com>
2025-01-01 23:23:24 +0530

    Day 01: Solved Two Sum :)

--- easy/0001-two-sum.js
+++ easy/0001-two-sum.js
@@ -1,9 +1,33 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
-var twoSum = function (nums, target) {
+/*********************************************************************************************
+ *                                   Problem: Two Sum                                        *
+ *                                   -----------------                                       *
+ *                                                                                           *
+ * Problem Description: Given an array of integers nums and an integer target, return        *
+ *                      indices of the two numbers such that they add up to target.          *
+ * Example:                                                                                  *
+ *    Input: nums = [2,7,11,15], target = 9                                                  *
+ *    Output: [0,1]                                                                          *
+ *********************************************************************************************/
+
+/*********************************************************************************************
+ *                                 Brute Force Approach                                      *
+ *********************************************************************************************/
+
+var twoSumBruteForce = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+};
+
+/*********************************************************************************************
+ *                                  Enhanced Approach                                        *
+ *********************************************************************************************/
+
+var twoSumEnhanced = function (nums, target) {
     let map = new Map();
 
     for (let i = 0; i < nums.length; i++) {
@@ -14,3 +38,15 @@ var twoSum = function (nums, target) {
         map.set(nums[i], i);
     }
 };
+
+/*********************************************************************************************
+ *                                 Time and Space Complexity                                 *
+ *********************************************************************************************/
+/* Brute Force Approach:                                                                    
+   - Time Complexity: O(n^2)                                                                 
+   - Space Complexity: O(1)                                                                 
+ 
+   Enhanced Approach:                                                                      
+   - Time Complexity: O(n)                                                                  
+   - Space Complexity: O(n)                                                                 
+ *********************************************************************************************/
